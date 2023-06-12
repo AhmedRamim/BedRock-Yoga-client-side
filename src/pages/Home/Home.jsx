7e468820e1568e3d0a8c1bc19d98ddd134989cef
AhmedRamim <ahmedramim821@gmail.com>
2023-06-12 21:10:13 +0600

    testimonial part complete

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Banner from './Banner';
+import Testimonial from '../Testimonial/Testimonial';
+
 
 const Home = () => {
     return (
         <div className=''>
-           <Banner></Banner>
+            <Banner></Banner>
+            <div className='container'>
+                <Testimonial />
+            </div>
         </div>
     );
 };
